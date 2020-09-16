114125462f493ee4bf24cde5d12a2498a6d88a50
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-16 16:55:33 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,12 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+  
+  x = []
 
   def initialize(brand)
     @brand = brand
+    if !(x.include?(@brand))
   end
 
   def cobble
